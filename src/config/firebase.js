5857dcc80bf8260a28e9d7651dfb134737684cda
JuanFc28 <juan.fc283@gmail.com>
2026-03-26 19:21:32 -0600

    Correccion de vulnerabilidad en credenciales

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,19 +4,19 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDpa7EWJZXeDSufHPr8f-2hEoBQF8d3iF4",
-    authDomain: "impulselab-5f11b.firebaseapp.com",
-    projectId: "impulselab-5f11b",
-    storageBucket: "impulselab-5f11b.firebasestorage.app",
-    messagingSenderId: "461010828388",
-    appId: "1:461010828388:web:a696fc6a795b9aa9a93371",
-    measurementId: "G-6M44YSTKNN",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: "G-6M44YSTKNN",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
+  persistence: getReactNativePersistence(AsyncStorage),
 });
 
 export const db = getFirestore(app);
